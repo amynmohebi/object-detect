d236eb1a1bcd88d1acafe2fc18ee5b046c80e55e
amin mohebi <135534807+amynmohebi@users.noreply.github.com>
2023-08-30 19:39:06 +0330

    ubject_detect.py

--- ubject_detect.py
+++ ubject_detect.py
@@ -1,12 +1,11 @@
 import cv2
-import numpy as np
 image=cv2.imread(r'your image location',0)    
 templat=cv2.imread(r'your templat location',0)
                             #template should be like your subject you wanna detect
 w,h=templat.shape
 result=cv2.matchTemplate(image,templat,cv2.TM_CCOEFF_NORMED)                                        
 threshuld=0.6
-         #Sensitivity value
+            #Sensitivity value
 locate=np.where(result>=threshuld)
 for point in zip(*locate[::-1]):
     cv2.rectangle(image,point,(point[0]+h,point[1]+w),(0.255,0),2)
